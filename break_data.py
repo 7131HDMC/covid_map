436e0aab7e279576ae57d6c84ea230a8330590ba
Hari Dasa <hari.moura@bernoulli.com.br>
2022-05-21 18:59:04 -0300

    fat | update csv file

--- break_data.py
+++ break_data.py
@@ -1,7 +1,7 @@
 import pandas as pd
 
 """## Importing the dataset and break export Brazil and States"""
-df = pd.read_csv("data/HIST_PAINEL_COVIDBR_2022_Parte1_09abr2022.csv", sep=";")
+df = pd.read_csv("data/HIST_PAINEL_COVIDBR_2022_Parte1_20mai2022.csv", sep=";")
 
 states_df = df[( (~df["estado"].isna()) & (df["codmun"].isna()) )]
 brazil_df = df[df["regiao"] == "Brasil"]
